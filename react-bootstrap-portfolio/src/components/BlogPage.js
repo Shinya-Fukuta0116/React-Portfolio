105551044623da256b48490a06d046529f8d9c4b
Shinya-Fukuta0116 <alleyezon.djwanted@gmail.com>
2023-12-12 04:28:45 +0900

    react-portfolio

---
+++ react-bootstrap-portfolio/src/components/BlogPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const BlogPage = () => {
+  return (
+    <div className='container'>
+      <h1>Blog</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora minus quo totam aut nam veniam. Voluptates aliquam labore accusamus possimus sit similique quae omnis cupiditate ad modi exercitationem molestias nesciunt, earum expedita quisquam nihil commodi voluptate quo, fuga tempore maiores eligendi magni magnam! Perferendis beatae inventore autem asperiores, unde illum iste consequuntur quam voluptatem. Enim rem quidem aspernatur placeat molestias deleniti praesentium aut fugit nulla veniam, ipsam hic ipsum in facere neque, recusandae architecto voluptates sapiente nesciunt minima tempora. Voluptate autem pariatur quas excepturi laudantium, deserunt repudiandae inventore fugiat quasi quae doloribus ipsa omnis rerum ratione, quisquam iste, ex aliquid.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora minus quo totam aut nam veniam. Voluptates aliquam labore accusamus possimus sit similique quae omnis cupiditate ad modi exercitationem molestias nesciunt, earum expedita quisquam nihil commodi voluptate quo, fuga tempore maiores eligendi magni magnam! Perferendis beatae inventore autem asperiores, unde illum iste consequuntur quam voluptatem. Enim rem quidem aspernatur placeat molestias deleniti praesentium aut fugit nulla veniam, ipsam hic ipsum in facere neque, recusandae architecto voluptates sapiente nesciunt minima tempora. Voluptate autem pariatur quas excepturi laudantium, deserunt repudiandae inventore fugiat quasi quae doloribus ipsa omnis rerum ratione, quisquam iste, ex aliquid.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora minus quo totam aut nam veniam. Voluptates aliquam labore accusamus possimus sit similique quae omnis cupiditate ad modi exercitationem molestias nesciunt, earum expedita quisquam nihil commodi voluptate quo, fuga tempore maiores eligendi magni magnam! Perferendis beatae inventore autem asperiores, unde illum iste consequuntur quam voluptatem. Enim rem quidem aspernatur placeat molestias deleniti praesentium aut fugit nulla veniam, ipsam hic ipsum in facere neque, recusandae architecto voluptates sapiente nesciunt minima tempora. Voluptate autem pariatur quas excepturi laudantium, deserunt repudiandae inventore fugiat quasi quae doloribus ipsa omnis rerum ratione, quisquam iste, ex aliquid.</p>
+    </div>
+  )
+}
+
+export default BlogPage
